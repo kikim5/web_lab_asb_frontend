4f14f91e1a78730ce1c90c1d60733c5c97a2197f
angelina kim <drabychev77@mail.ru>
2024-01-19 05:08:18 +0300

    Лабораторная работа 12

---
+++ labs/Lab_12_python_intro/solution/task2.py
@@ -0,0 +1,20 @@
+import heapq
+
+
+def find_median(n, arr):
+    max_sq, min_sq, result = [], [], 0
+
+    for i in range(n):
+        heapq.heappush(max_sq, -arr[i])
+        heapq.heappush(min_sq, -heapq.heappop(max_sq))
+
+        if len(min_sq) > len(max_sq):
+            heapq.heappush(max_sq, -heapq.heappop(min_sq))
+        result -= max_sq[0]
+
+    return result
+
+
+if __name__ == '__main__':
+    n, arr = int(input()), list(map(int, input().split()))
+    print(find_median(n, arr))
